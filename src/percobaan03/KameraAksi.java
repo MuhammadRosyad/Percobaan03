b70199f10bc11c5b94455adfe2c02f846a70882e
MuhammadRosyad <achaibuy01@gmail.com>
2018-04-08 23:58:46 +0800

    Menggunakan Method GET

--- src/percobaan03/KameraAksi.java
+++ src/percobaan03/KameraAksi.java
@@ -6,6 +6,14 @@ public class KameraAksi {
         k.setMerk("Canon");
         k.setMegapixel("20");
         k.setHarga("5000");
+        
         k.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(k.getMerk());
+        System.out.print("Megapixelnya \t: ");
+        System.out.println(k.getMegapixel());
+        System.out.print("Harganya \t: ");
+        System.out.println(k.getHarga());
     }
 }
