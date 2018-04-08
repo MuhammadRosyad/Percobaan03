eb76ad3ff45374e5025b3424ef86fc1eebf74a9b
MuhammadRosyad <achaibuy01@gmail.com>
2018-04-08 23:30:30 +0800

    Buat method cetakInfo

--- src/percobaan03/Kamera.java
+++ src/percobaan03/Kamera.java
@@ -3,4 +3,10 @@ public class Kamera {
     String Merk;
     String Megapixel;
     Double Harga;
+    
+    void cetakInfo(){
+        System.out.println("Merk \t: "+Merk+"\n"+
+                "Megapixel \t: "+Megapixel+"\n"+
+                "Harga \t:"+Harga);
+    }
 }
