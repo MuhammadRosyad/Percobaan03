52fc9e808d4796ec870ea374756a2851f276e7f3
MuhammadRosyad <achaibuy01@gmail.com>
2018-04-08 23:26:46 +0800

    Membuat Atribut

--- src/percobaan03/Kamera.java
+++ src/percobaan03/Kamera.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package percobaan03;
-
-/**
- *
- * @author Om Genocide
- */
 public class Kamera {
-    
+    String Merk;
+    String Megapixel;
+    Double Harga;
 }
